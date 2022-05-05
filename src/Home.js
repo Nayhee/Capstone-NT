ec39ed9da4cffe574a60d0223eafa67ed6084e94
Nayhee <nathantraczewski@gmail.com>
2022-05-05 13:16:47 -0500

    finished new Circle Scorecardgit add --all kept buttons in middle. About to fix new round data issue

--- src/Home.js
+++ src/Home.js
@@ -22,21 +22,7 @@ export const Home = () => {
     const [filteredRounds, setFilteredRounds] = useState([]);
     const [allUsersRounds, setAllUsersRounds] = useState([]);
 
-
-    //useEffect with AllUsersRounds as dependency.
-
-    //loop through all rounds, and 
-
-    let c1 = []
-    let c2 = []
-    let c3 = []
-
-
-
-
-
-
-
+    const [circle, setCircle] = useState({})
 
 
     //helper function to turn decimal into a percentage.
@@ -122,22 +108,102 @@ export const Home = () => {
         scorecardCalcs(filteredRounds)
     }, [filteredRounds])
 
+    useEffect(() => {
+        circleCalcs();
+    }, [allUsersRounds])
+
+
+
+    const circleCalcs = () => {
+
+        let circleOne = []
+        let circleTwo = []
+        let circleThree = []
+    
+        allUsersRounds.forEach(round => {
+            
+            if(round.distance < 34 && round.distance > 0) {
+                circleOne.push(round);
+            
+            }
+            if(round.distance > 33 && round.distance < 67) {
+                circleTwo.push(round);
+            }
+            if(round.distance > 66) {
+                circleThree.push(round);
+            }
+        })
+
+
+        let c1Putts = 0;
+        let c1Made = 0;
+        let c2Putts = 0;
+        let c2Made = 0;
+        let c3Putts = 0;
+        let c3Made = 0;
+    
+        circleOne.forEach(round => {
+            c1Putts += round.putts;
+            c1Made += round.made;
+        })
+        const c1percent = c1Made / c1Putts;
+    
+        circleTwo.forEach(round => {
+            c2Putts += round.putts;
+            c2Made += round.made;
+        })
+        const c2percent = c2Made / c2Putts;
+    
+        circleThree.forEach(round => {
+            c3Putts += round.putts;
+            c3Made += round.made;
+        })
+        const c3percent = c3Made / c3Putts;
+    
+        let c1percentage = decimalToPercentage(c1percent)
+        let c2percentage = decimalToPercentage(c2percent)
+        let c3percentage = decimalToPercentage(c3percent)
+
+        if(isNaN(c1percentage)) {
+            c1percentage = " - ";
+        } else {
+            c1percentage = `${c1percentage}%`
+        }
+        if(isNaN(c2percentage)) {
+            c2percentage = " - ";
+        } else {
+            c2percentage = `${c2percentage}%`
+        }
+        if(isNaN(c3percentage)) {
+            c3percentage = " - ";
+        } else {
+            c3percentage = `${c3percentage}%`
+        }
+
+        const circleStatsObj = {
+            c1: c1percentage,
+            c2: c2percentage,
+            c3: c3percentage
+        }
+        setCircle(circleStatsObj)
+    }
+
 
     return (
         <>
             <div className="home__container">
 
                     <div className="circleScorecard">
-                        <h4>% by Distance:</h4>
+                        <h4>% Made by Distance:</h4>
                         <div className="circleWrapper">
                             <div className="circleItemLabel">Distance:</div>
                             <div className="circleItemLabel">% Made:</div>
                             <div className="circleItem">1-33 ft</div>
-                            <div className="circleItem">c1</div>
+                            <div className="circleItem">{circle.c1}</div>
                             <div className="circleItem">33-66 ft</div>
-                            <div className="circleItem">c2</div>
+                            <div className="circleItem">{circle.c2}</div>
                             <div className="circleItem">66+ ft</div>
-                            <div className="circleItem">c3</div>
+                            <div className="circleItem">{circle.c3}</div>
                         </div>
                     </div>
                     
